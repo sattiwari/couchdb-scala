076d3c3fa064daf3688275b163e2de9d4d781724
Satendra Tiwari <sattiwari18@gmail.com>
2017-03-16 11:57:26 +0700

    spec config

--- src/test/scala/com/stlabs/couchdb/spec/SpecConfig.scala
+++ src/test/scala/com/stlabs/couchdb/spec/SpecConfig.scala
@@ -1,5 +1,14 @@
 package com.stlabs.couchdb.spec
 
 object SpecConfig {
+  val couchDbHost   = System.getProperty("couchDbHost", "127.0.0.1")
+  val couchDbPort   = System.getProperty("couchDbPort", "5984").toInt
+  val couchDbScheme = System.getProperty("couchDbScheme", "http")
 
+  private val log = org.log4s.getLogger
+
+  log.info("----------------------")
+  log.info(s"couchDbHost: $couchDbHost")
+  log.info(s"couchDbPort: $couchDbPort")
+  log.info("----------------------")
 }
